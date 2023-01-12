e9b2237eac98a2275172677834671f8b37940d03
Grace Gong <ggong@lcs.on.ca>
2023-01-12 11:36:03 -0500

    Initial Commit

---
+++ LayoutExecise/LayoutExeciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExeciseApp.swift
+//  LayoutExecise
+//
+//  Created by Grace Gong on 2023-01-12.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExeciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
